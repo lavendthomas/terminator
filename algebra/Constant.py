6a298860ead76086f2bb4b5da07d122eff44cb4e
Thomas Lavend'Homme <lavendthomas@posteo.net>
2018-12-12 11:15:22 +0100

    Add types for constants

--- algebra/Constant.py
+++ algebra/Constant.py
@@ -3,5 +3,15 @@ class Constant:
     def __init__(self, cst):
         self.cst = str(cst)
 
+        if isinstance(cst, int):
+            self.type = "INT"
+        elif isinstance(cst, float):
+            self.type = "FLOAT"
+        elif isinstance(cst, str):
+            self.type = "TEXT"
+
+    def get_type(self):
+        return self.type
+
     def toSQL(self, dbschema):
         return '"' + self.cst + '"'
