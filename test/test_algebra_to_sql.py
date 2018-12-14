8cf468a376d01a30e104e2fef13fbab0b784990c
Thomas Lavend'Homme <lavendthomas@posteo.net>
2018-12-14 11:51:37 +0100

    Added unit tests

--- test/test_algebra_to_sql.py
+++ test/test_algebra_to_sql.py
@@ -10,6 +10,7 @@ from algebra.Join import Join
 from algebra.Union import Union
 from algebra.Difference import Difference
 from remote.DBSchema import DBSchema
+from algebra.Exceptions import *
 
 
 class TestAlgebraToSQL(unittest.TestCase):
@@ -30,6 +31,19 @@ class TestAlgebraToSQL(unittest.TestCase):
         # Test that the selection doesn't change the attributes
         self.assertEqual(query.get_attributes(self.db).sort(), Relation("CITIES").get_attributes(self.db).sort())
 
+    def test_selection_constant_attribute_not_in_table(self):
+        # Test if the attribute is not in the table
+        badquery = SelectionConstant(Attribute("notacity"), Constant("Mons"), Relation("CITIES"))
+        with self.assertRaises(InvalidAttributeException):
+            badquery.toSQL(self.db)
+
+    def test_selection_constant_constant_different_type_as_attribute(self):
+        # Test if the constant is not the same type as the attribute
+        badquery = SelectionConstant(Attribute("city"), Constant(1), Relation("CITIES"))
+        with self.assertRaises(DifferentTypeException):
+            badquery.toSQL(self.db)
+
+
     def test_selection_attribute(self):
         query = SelectionAttribute(Attribute("id"), Attribute("name"), Relation("users"))
         self.assertEqual(query.toSQL(self.db),"SELECT * FROM users WHERE id = name")
@@ -37,3 +51,8 @@ class TestAlgebraToSQL(unittest.TestCase):
         # Test that the selection doesn't change the attributes
         self.assertEqual(query.get_attributes(self.db).sort(), Relation("users").get_attributes(self.db).sort())
 
+    def test_selection_attribute_attribute_not_in_table(self):
+        # Test if the attribute is not in the table
+        badquery = SelectionAttribute(Attribute("id"), Attribute("name"), Relation("users")
+        with self.assertRaises(InvalidAttributeException):
+            badquery.toSQL(self.db)
